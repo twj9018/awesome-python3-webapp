49c595f2e9f872d3d1f09a0283fde913a01804bb
tangwenjun <tangwenjun@skybility.com>
2016-11-29 18:07:51 +0800

    add LICENSE

---
+++ dist/app.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+import asyncio
+import os
+import json
+import time
+from datetime import datetime
+from aiohttp import web
+
+def index(request):
+    return web.Reque
